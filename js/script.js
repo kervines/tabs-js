e5fb18ea7651c5c258efb84745f3570d58d268cc
kervines <kervinzero@gmail.com>
2023-12-30 15:08:57 -0300

    feat - criada as funcionalidades dos marcadores das tab, a mudança de texto e a mudança das imagens

--- js/script.js
+++ js/script.js
@@ -1,10 +1,35 @@
-const tabList = document.querySelectorAll('[data-tab]');
-const sectionContents = document.querySelectorAll('[tab-content]');
+function initTabSection() {
+  const tabList = document.querySelectorAll('[data-tab]');
+  const sectionContents = document.querySelectorAll('[data-content]');
+  const imagesEl = document.querySelectorAll('.image-container img');
 
-tabList.forEach((tab) => {
-  tab.addEventListener('click', handleClickTab);
-});
+  function activeContent(index) {
+    sectionContents.forEach((content) => {
+      content.classList.add('desable');
+    });
+    sectionContents[index].classList.remove('desable');
+  }
 
-function handleClickTab() {
-  this.classList.add('active');
+  function activeImage(index) {
+    imagesEl.forEach((img) => {
+      img.classList.add('desable');
+    });
+    imagesEl[index].classList.remove('desable');
+  }
+
+  function activeTabs(event) {
+    for (let i = 0; i < tabList.length; i++) {
+      tabList[i].classList.remove('active');
+    }
+    event.target.classList.add('active');
+  }
+
+  tabList.forEach((tab, index) => {
+    tab.addEventListener('click', (event) => {
+      activeTabs(event);
+      activeContent(index);
+      activeImage(index);
+    });
+  });
 }
+initTabSection();
